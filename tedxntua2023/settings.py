9a85d24acad537b2c753073cea491233b96043d3
George Papadoulis <72205342+G-Papad@users.noreply.github.com>
2022-01-04 16:09:43 +0200

    Update settings.py

--- tedxntua2023/settings.py
+++ tedxntua2023/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE':  'django.db.backends.mysql',
         'NAME': 'tedxntua2022_database',
         'USER': 'root',
-        'PASSWORD': 'geo%pap$0595*',
+        'PASSWORD': '****',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
