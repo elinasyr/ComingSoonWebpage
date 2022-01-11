ac854f5b5d1698132e0fb7f2ac70a3ad674fd981
ElinaSyr <elinasyrr@gmail.com>
2022-01-11 10:57:23 +0200

    Responsive Header

---
+++ main/static/main/js/main.js
@@ -0,0 +1,19 @@
+console.log("hi"); 
+console.log("FIND me"); 
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+// const navLink = document.querySelectorAll(".nav-link");
+
+// hamburger.addEventListener("click", mobileMenu);
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+//     console.log("HI");
+// }
+
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
