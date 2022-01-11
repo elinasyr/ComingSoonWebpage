f5a1b2141783e6a251e6ff2a6f69b1c68368cb61
ElinaSyr <elinasyrr@gmail.com>
2022-01-11 11:30:29 +0200

    Responsive Header

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,19 +1,16 @@
-console.log("hi"); 
-console.log("FIND me"); 
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-// const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
-// hamburger.addEventListener("click", mobileMenu);
-// navLink.forEach(n => n.addEventListener("click", closeMenu));
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-// function mobileMenu() {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-//     console.log("HI");
-// }
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
 
-// function closeMenu() {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-// }
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
