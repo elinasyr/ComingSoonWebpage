33a1d7c30063a049592376f1d24ca52af96408eb
ElinaSyr <elinasyrr@gmail.com>
2022-01-12 21:35:24 +0200

    About us

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const navActive = document.querySelectorAll(".nav-menu.active"); 
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -14,3 +15,7 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// window.addEventListener('resize', function(event) {
+//     navActive.style.marginTop = 5; 
+// }, true);
